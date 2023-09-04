16728dd51ab9ea5195e886b49e17290705e974c3
larama-C <56674013+larama-C@users.noreply.github.com>
2023-09-04 17:15:50 +0900

    0904 17:15

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Animator anim;
     public int playerSpeed = 1;
     Rigidbody2D rid2D;
     SpriteRenderer rend;
@@ -11,8 +12,10 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponentInChildren<Animator>();
         rid2D = GetComponent<Rigidbody2D>();
-        rend = GetComponent<SpriteRenderer>();
+        rend = GetComponentInChildren<SpriteRenderer>();
+
     }
 
     // Update is called once per frame
@@ -25,15 +28,27 @@ public class PlayerController : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.RightArrow))
         {
+            anim.SetBool("IsWalk", true);
             rend.flipX = true;
             rid2D.AddForce(new Vector2(playerSpeed, 0), ForceMode2D.Force);
             //transform.Translate(new Vector3(playerSpeed * Time.deltaTime, 0, 0));
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.LeftArrow))
         {
+            anim.SetBool("IsWalk", true);
             rend.flipX = false;
             rid2D.AddForce(new Vector2(-playerSpeed, 0), ForceMode2D.Force);
             //transform.Translate(new Vector3(-playerSpeed * Time.deltaTime, 0, 0));
+        }
+        else if(Input.GetKey(KeyCode.LeftAlt))
+        {
+            rid2D.AddForce(new Vector2(0, 2), ForceMode2D.Force);
+            anim.SetBool("IsJump", true);
+        }
+        else
+        {
+            anim.SetBool("IsJump", false);
+            anim.SetBool("IsWalk", false);
         }
     }
 }
